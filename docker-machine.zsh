5b2985097de372ed1c83567a8eeb8b7568a152b7
Joe Scaduto <jscaduto@mysidewalk.com>
2015-12-23 11:14:46 -0600

    add docker-machine aliases

--- docker-machine.zsh
+++ docker-machine.zsh
@@ -1,3 +1,10 @@
 # Docker-machine
 eval "$(docker-machine env default)"
 export DOCKER_HOST_IP="$(docker-machine ip default)"
+
+# Docker-machine
+alias dme="docker-machine env default"
+alias dmk="docker-machine kill default"
+alias dmr="docker-machine restart default"
+alias dmrm="docker-machine rm default"
+alias dms="docker-machine start default"
